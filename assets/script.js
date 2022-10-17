a19fc5f3f03d42316b475f995907606f3e50fd55
Jason Charney <jrcharney@gmail.com>
2022-10-17 01:04:08 -0500

    Adding JavaScript to the PSG

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+/* File: script.js
+ * Info: add functionality to our HTML page.
+ * Note: Like with my CSS file, expect some opinions.
+ */
+
+// HEY! Stop using "var" for your variables! Use "let".
+//let topics = "HTML, CSS, Git, JavaScript";
+// console.log(topics);
+
+let topics = ["HTML", "CSS", "Git", "JavaScript"];
+
+for(let x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+}
+
+
+// let topic = 'HTML';
+
+/*
+if(topic === 'HTML'){
+    console.log("Let's study HTML!");
+}else if(topic === 'CSS'){
+    console.log("Let's study CSS!")
+}else if(topic === 'Git'){
+    console.log("Let's study Git!")
+}else if(topic === 'JavaScript'){
+    console.log("Let's study JavaScript!")
+}else{
+    console.log("Please try again!");
+}
+*/
